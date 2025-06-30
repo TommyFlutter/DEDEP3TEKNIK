9de85e748c57bad06224fb1f9ada00dad7251c4c
Tommy Azwar Muslim <azwarmuslimtommy@gmail.com>
2025-06-30 21:11:00 +0700

    upadate DIST

--- src/scripts/views/pages/daftar-resto.js
+++ src/scripts/views/pages/daftar-resto.js
@@ -9,6 +9,9 @@ const DaftarResto = {
     
       <div class="content">
         <h2 class="content__heading">Daftar Produk MACINNING + PABRICASION</h2>
+        <h3>Alamat pada peta, Tepat di depan Indomaret.MBJ</h3>  
+       <iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Indomaret.MBJ%20Bekasi+(Indomaret.MBJ)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='http://mapseinbinden.org'>google map einbinden</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=5010edb6be0de404a8a688adb338d3bbe218f252'></script> 
+
         <div id="restaurant-list" class="restaurant-list">
         </div>
       </div>
